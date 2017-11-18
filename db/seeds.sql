7a6859e0821f57f4d3d226ad9fc80eccda2a5ac7
Kaylynn <kaylynn.rothleder@gmail.com>
2017-11-17 22:52:34 -0800

    test

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers(burger_name, devoured, date)
+VALUES ("Big Mac", false, "2017-11-05 04:20:00"),
+  ("Whooper", false, "2017-11-05 04:20:01"),
+  ("Cheezburger", false, "2017-11-05 04:20:02"),
+  ("Baconator", false, "2017-11-05 16:20:00")
